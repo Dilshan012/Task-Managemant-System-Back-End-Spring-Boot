935f4c09937031cc9a4f3c5a3b1dac05c353d9e3
Dilshan012 <dilnet.it@gmail.com>
2024-06-11 08:55:59 +0530

    2nd Commit

---
+++ src/main/java/com/fullstackapp/taskmanagemantsystem/TaskmanagemantsystemApplication.java
@@ -0,0 +1,13 @@
+package com.fullstackapp.taskmanagemantsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagemantsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmanagemantsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fullstackapp/taskmanagemantsystem/TaskmanagemantsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstackapp.taskmanagemantsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagemantsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
